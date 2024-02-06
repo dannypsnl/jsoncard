bf0c8fc8beb1c0900e002210b55732c085b33ddc
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2024-02-06 12:41:42 +0800

    ppx
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- bin/main.ml
+++ bin/main.ml
@@ -7,6 +7,16 @@ let extract_list json =
   | `List l -> l
   | _ -> []
 
+let author s = "\\author{" ^ s ^ "}"
+
+type arxivcard = {
+  date: string;         [@printer fun fmt -> fprintf fmt "\\date{%s}"]
+  title: string;        [@printer fun fmt -> fprintf fmt "\\title{%s}"]
+  authors: string list; [@printer fun fmt a -> List.map author a |> String.concat "" |> fprintf fmt "%s"]
+  doi: string;          [@printer fun fmt -> fprintf fmt "\\meta{doi}{%s}"]
+  abstract: string      [@printer fun fmt -> fprintf fmt "\\p{%s}"]
+} [@@deriving show]
+
 let () =
   let stdin = Stdio.stdin in
   let cardinfo = Yojson.Basic.from_channel stdin in
@@ -17,10 +27,14 @@ let () =
       let authors = List.assoc "authors" m |> extract_list |> List.map extract_string in
       let doi = List.assoc "doi" m |> extract_string in
       let abstract = List.assoc "abstract" m |> extract_string in
-      print_endline date;
-      print_endline title;
-      print_endline doi;
-      print_endline abstract;
+      let card : arxivcard = {
+        date;
+        title;
+        authors;
+        doi;
+        abstract
+      } in
+      print_endline (show_arxivcard card)
   | _ ->
       print_string "Error: cardinfo is not an object";
       print_endline (Yojson.Basic.to_string cardinfo)
